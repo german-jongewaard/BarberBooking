ae71c781f04fdb78f8d8554d5a3f6cfc79a11d89
german-jongewaard <germanjongewaard@gmail.com>
2019-05-25 16:20:16 +0200

    Interface

---
+++ app/src/main/java/com/jongewaard/dev/barberbooking/Interface/IBookingInfoLoadListener.java
@@ -0,0 +1,9 @@
+package com.jongewaard.dev.barberbooking.Interface;
+
+import com.jongewaard.dev.barberbooking.Model.BookingInformation;
+
+public interface IBookingInfoLoadListener {
+    void onBookingInfoLoadEmpty();
+    void onBookingInfoLoadSuccess(BookingInformation bookingInformation, String documentId);
+    void onBookingInfoLoadFailed(String message);
+}
