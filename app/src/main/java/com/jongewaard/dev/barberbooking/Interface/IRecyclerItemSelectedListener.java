9a0207f06688b62e53c0df78211cc0334e589383
german-jongewaard <germanjongewaard@gmail.com>
2019-05-29 16:23:12 +0200

    Interface

---
+++ app/src/main/java/com/jongewaard/dev/barberbooking/Interface/IRecyclerItemSelectedListener.java
@@ -0,0 +1,7 @@
+package com.jongewaard.dev.barberbooking.Interface;
+
+import android.view.View;
+
+public interface IRecyclerItemSelectedListener {
+    void onitemSelectedListener(View view, int pos);
+}
